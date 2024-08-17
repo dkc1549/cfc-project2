53db4d61103eae47281de9b6ce76c6e74c5b2c52
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-08-17 11:19:00 +0545

    update

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,10 +1,47 @@
 @extends('app')
-@section('title',"Blog/Index")
+@section('title', 'Blog/Index')
 @section('main-section')
-<header>
-    <h1 class="text-center text-primary">Blogs</h1>
-</header>
-<div class="container">
-
-</div>
+    <header>
+        <h1 class="text-center text-primary">Blogs</h1>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/blogs/view.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+@section('title',"Blogs / Create")
+@section('main-section')
+
+@endsection
